a656d1eb440d1e9859ea6f0167b8d0f2e22f8289
Daniel Pham <sonph@vnext.com.vn>
2021-04-06 22:52:48 +0700

    Inherit ERC20Capped, ERC20Burnable

--- contracts/SoneToken.sol
+++ contracts/SoneToken.sol
@@ -2,92 +2,77 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-// SoneToken with Governance.
-contract SoneToken is ERC20("SONE Token", "SONE"), Ownable {
-    uint256 private _cap = 100000000e18;
-    uint256 private _totalLock;
+// SONE Token based on ERC-20 standard
+contract SoneToken is ERC20, ERC20Capped, ERC20Burnable, Ownable {
+    uint256 private _cap = 100000000e18;    // Maximum supply
+    uint256 private _totalLock;             // Pre-caculate total locked SONE tokens
 
-    uint256 public lockFromBlock;
-    uint256 public lockToBlock;
+    uint256 public lockFromBlock;           // Block number that SONE token is locked from
+    uint256 public lockToBlock;             // Block number that SONE token is locked to
 
-    mapping(address => uint256) private _locks;
-    mapping(address => uint256) private _lastUnlockBlock;
+    mapping(address => uint256) private _locks;             // Current locked SONE of each address
+    mapping(address => uint256) private _lastUnlockBlock;   // The last block number that a address's SONE is unlocked
 
     event Lock(address indexed to, uint256 value);
 
-    constructor(uint256 _lockFromBlock, uint256 _lockToBlock) {
-        lockFromBlock = _lockFromBlock;
-        lockToBlock = _lockToBlock;
+    constructor(uint256 lockFromBlock_, uint256 lockToBlock_) 
+    ERC20("SONE Token", "SONE")
+    ERC20Capped(_cap) {
+        lockFromBlock = lockFromBlock_;
+        lockToBlock = lockToBlock_;
     }
 
     /**
-     * @dev Returns the cap on the token's total supply.
+     * @dev Return current circulatable SONE tokens.
      */
-    function cap() public view returns (uint256) {
-        return _cap;
-    }
-
     function circulatingSupply() public view returns (uint256) {
-        return totalSupply() - _totalLock;
+        return super.totalSupply() - _totalLock;
     }
 
+    /**
+     * @dev Return total locked SONE tokens.
+     */
     function totalLock() public view returns (uint256) {
         return _totalLock;
     }
 
     /**
-     * @dev See {ERC20-_beforeTokenTransfer}.
-     *
-     * Requirements:
-     *
-     * - minted tokens must not cause the total supply to go over the cap.
+     * @dev See {ERC20-_mint}.
+     * Can only be called by the current owner.
      */
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
-        super._beforeTokenTransfer(from, to, amount);
-
-        if (from == address(0)) { // When minting tokens
-            require(totalSupply() + amount <= _cap, "ERC20Capped: cap exceeded");
-        }
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        super._mint(_to, _amount);
     }
 
     /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
+     * @dev Return total SONE balance of a address.
      */
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
-        super._transfer(sender, recipient, amount);
-    }
-
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner.
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        _mint(_to, _amount);
-    }
-
     function totalBalanceOf(address _holder) public view returns (uint256) {
         return _locks[_holder] + balanceOf(_holder);
     }
 
+    /**
+     * @dev Return locked SONE token of a address.
+     */
     function lockOf(address _holder) public view returns (uint256) {
         return _locks[_holder];
     }
 
+    /**
+     * @dev Return block number that contains the last unlock call by a address.
+     */
     function lastUnlockBlock(address _holder) public view returns (uint256) {
         return _lastUnlockBlock[_holder];
     }
 
+    /**
+     * @dev Lock a SONE token amount of a address.
+     */
     function lock(address _holder, uint256 _amount) public onlyOwner {
         require(_holder != address(0), "ERC20: lock to the zero address");
         require(_amount <= balanceOf(_holder), "ERC20: lock amount over blance");
@@ -102,6 +87,9 @@ contract SoneToken is ERC20("SONE Token", "SONE"), Ownable {
         emit Lock(_holder, _amount);
     }
 
+    /**
+     * @dev Return true if an address can unlock SONE token.
+     */
     function canUnlockAmount(address _holder) public view returns (uint256) {
         if (block.number < lockFromBlock) {
             return 0;
@@ -116,6 +104,9 @@ contract SoneToken is ERC20("SONE Token", "SONE"), Ownable {
         }
     }
 
+    /**
+     * @dev Unlock SONE token for the sender
+     */
     function unlock() public {
         require(_locks[msg.sender] > 0, "ERC20: cannot unlock");
         
@@ -130,7 +121,9 @@ contract SoneToken is ERC20("SONE Token", "SONE"), Ownable {
         _totalLock = _totalLock - amount;
     }
 
-    // This function is for dev address migrate all balance to a multi sig address
+    /**
+     * @dev Dev address migrate all balance to a multi sig address
+     */
     function transferAll(address _to) public {
         _locks[_to] = _locks[_to] + _locks[msg.sender];
 
@@ -146,5 +139,12 @@ contract SoneToken is ERC20("SONE Token", "SONE"), Ownable {
         _lastUnlockBlock[msg.sender] = 0;
 
         _transfer(msg.sender, _to, balanceOf(msg.sender));
+    }
+
+    /**
+     * @dev See {ERC20Capped-_mint}.
+     */
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
+        super._mint(account, amount);
     }
 }
