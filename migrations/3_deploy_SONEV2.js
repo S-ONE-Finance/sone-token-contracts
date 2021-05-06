bef3b010f67eedff72af156ca92a01e81df7a2f3
Daniel Pham <sonph@vnext.com.vn>
2021-05-06 10:35:34 +0700

    Update precompile command

--- migrations/3_deploy_SONEV2.js
+++ migrations/3_deploy_SONEV2.js
@@ -2,9 +2,9 @@ const SoneTokenV2 = artifacts.require("SoneTokenV2");
 
 const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
-const addressAdminUpgradable = '0x1970c7cF311F22F9d90e15CdEaf2E6Fd0536CACC';
+const addressAdminUpgradable = '0x7b6f9B472123DB1B38b18a713F3FCD6d4fcf90d7';
 
 module.exports = async function (deployer) {
-  // const adminUpgradable = await upgradeProxy(addressAdminUpgradable, SoneTokenV2, { deployer });
-  // console.log('adminUpgradable', adminUpgradable);
+  const adminUpgradable = await upgradeProxy(addressAdminUpgradable, SoneTokenV2, { deployer });
+  console.log('adminUpgradable', adminUpgradable);
 };
