3ec2e292d928c08867d6472cf02a31c5ca8e6452
Daniel Pham <sonph@vnext.com.vn>
2021-05-12 03:10:36 +0700

    Estimate gasLimit to deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations, {
+    gas: 136112
+  });
 };
